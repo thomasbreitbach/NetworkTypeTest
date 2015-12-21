50732d383bb3da7e4d235aad7eefb04b3feecd8b
Thomas Breitbach <thomas.breitbach@iem.thm.de>
2015-12-21 10:39:23 +0100

    getMobileNetworkType

--- app/src/main/java/de/tbreitbach/android/networktypetest/MainActivity.java
+++ app/src/main/java/de/tbreitbach/android/networktypetest/MainActivity.java
@@ -1,13 +1,74 @@
 package de.tbreitbach.android.networktypetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TelephonyManager mTeleManager;
+
+    // UI
+    private TextView mSubtype;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubtype = (TextView) findViewById(R.id.tv_hello);
+        mTeleManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mSubtype.setText(getMobileNetworkType(mTeleManager));
+    }
+
+    private String getMobileNetworkType(TelephonyManager manager){
+        switch(manager.getNetworkType()){
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return "1xRTT";
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return "CDMA"; // ~ 14-64 kbps
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return "EDGE"; // ~ 50-100 kbps
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return "EVDO_0"; // ~ 400-1000 kbps
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return "EVDO_A"; // ~ 600-1400 kbps
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return "GPRS"; // ~ 100 kbps
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return "HSDPA"; // ~ 2-14 Mbps
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return "HSPA"; // ~ 700-1700 kbps
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return "HSUPA"; // ~ 1-23 Mbps
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return "UMTS"; // ~ 400-7000 kbps
+
+            /*
+			 * Above API level 7, make sure to set android:targetSdkVersion
+			 * to appropriate level to use these
+			 */
+            case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
+                return "EHRPD"; // ~ 1-2 Mbps
+            case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
+                return "EVDO_B"; // ~ 5 Mbps
+            case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
+                return "HSPAP"; // ~ 10-20 Mbps
+            case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
+                return "IDEN"; // ~25 kbps
+            case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
+                return "LTE"; // ~ 10+ Mbps
+            // Unknown
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+            default:
+                return "UNKNOWN";
+        }
     }
 }
